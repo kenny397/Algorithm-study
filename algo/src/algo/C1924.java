c0d36c6ee4354a0017be2ff816f1f839bce0d462
kenny <kenny397@naver.com>
2021-05-26 09:46:52 +0900

    크게 만들기

---
+++ algo/src/algo/C1924.java
@@ -0,0 +1,41 @@
+package algo;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class C1924 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		String t=sc.next();
+		char[]arr=t.toCharArray();
+		Deque<Character>st=new ArrayDeque<>();
+		
+		
+		for(int i=0; i<n;i++) {
+			
+			while(k>0&&!st.isEmpty()&&st.getLast()<arr[i]) {
+				
+				st.removeLast();
+				k--;
+				
+				
+			} 
+			
+			st.addLast(arr[i]);
+			
+		}StringBuilder ans = new StringBuilder();
+		
+		while(st.size()>k) {
+			ans.append(st.removeFirst());
+		}
+		
+		System.out.println(ans.toString());
+	}
+
+}
