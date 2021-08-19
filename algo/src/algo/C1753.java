5cd775271e4bb6d5d306c5947cf7522ed7df2cdb
kenny <kenny397@naver.com>
2021-08-19 12:10:10 +0900

    최단경로(최적화)

--- algo/src/algo/C1753.java
+++ algo/src/algo/C1753.java
@@ -1,64 +1,74 @@
 package algo;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class C1753 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		int v=sc.nextInt();
-		int e=sc.nextInt();
-		int start=sc.nextInt();
-		ArrayList<Edge>[] adj=new ArrayList[v+1];
-		for(int i=1; i<=v; i++)adj[i]=new ArrayList<>();
-		for(int i=0; i<e; i++) {
-			int u=sc.nextInt();
-			int u2=sc.nextInt();
-			int w=sc.nextInt();
-			adj[u].add(new Edge(u2,w));
+	public static void main(String[] args)throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		int V=Integer.parseInt(st.nextToken());
+		int E=Integer.parseInt(st.nextToken());
+		int start=Integer.parseInt(br.readLine());
+		ArrayList<Node>[]al=new ArrayList[V+1];
+		for(int i=1; i<=V; i++)
+		al[i]=new ArrayList<>();
+		for(int i=0; i<E; i++) {
+			st=new StringTokenizer(br.readLine());
+			int u=Integer.parseInt(st.nextToken());
+			int v=Integer.parseInt(st.nextToken());
+			int w=Integer.parseInt(st.nextToken());
+			al[u].add(new Node(v,w));
 		}
-		boolean[] c=new boolean[v+1];
-		int[] dist=new int[v+1];
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		pq.add(new Edge(start,0));
-		dist[start]=0;
+		int[] distance=new int[V+1];
+		boolean[] visit=new boolean[V+1];
+		Arrays.fill(distance, Integer.MAX_VALUE);
+		distance[start]=0;
+		PriorityQueue<Node> pq=new PriorityQueue<>();
+		pq.add(new Node(start,distance[start]));
 		while(!pq.isEmpty()) {
-			Edge edge=pq.poll();
-			int destination=edge.v;
-			if(c[destination])continue;
-			c[destination]=true;
-			for(Edge next : adj[destination]) {
-				if(dist[destination]+next.w<=dist[next.v]) {
-					dist[next.v]=dist[destination]+next.w;
-					pq.add(new Edge(next.v,dist[next.v]));
+			Node node=pq.poll();
+			if(visit[node.v])continue;
+			visit[node.v]=true;
+			for(Node el: al[node.v]) {
+				if(distance[el.v]>node.w+el.w) {
+					distance[el.v]=node.w+el.w;
+					pq.add(new Node(el.v,distance[el.v]));
 				}
 			}
-		}
-		for(int i=1; i<dist.length; i++) {
-			if(dist[i]==Integer.MAX_VALUE)
-			System.out.println("INF");
-			else System.out.println(dist[i]);
-		}
+			
+		}for(int i=1; i<=V; i++) {
+				if(distance[i]==Integer.MAX_VALUE)System.out.println("INF");
+				else System.out.println(distance[i]);
+			}
+			
+		
+		
+		
+		
+		
 		
 	}
-	static class Edge implements Comparable<Edge>{
-		int v;
-		int w;
-		public Edge(int v, int w) {
+	static class Node implements Comparable<Node>{
+		int v,w;
+		
+		public Node(int v, int w) {
+			super();
 			this.v = v;
 			this.w = w;
 		}
+
 		@Override
-		public int compareTo(Edge o) {
+		public int compareTo(Node o) {
 			// TODO Auto-generated method stub
-			return Integer.compare(this.w, o.w);
+			return this.w-o.w;
 		}
 		
 	}
-
 }
