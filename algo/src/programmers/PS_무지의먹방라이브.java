9211bce3e2601bf11619c3cb440219cac9fb4bbf
kenny <kenny397@naver.com>
2021-08-21 16:57:31 +0900

    2019 kakao

---
+++ algo/src/programmers/PS_무지의먹방라이브.java
@@ -0,0 +1,56 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class PS_무지의먹방라이브 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]f= {1,1,1,1};
+		int k= 4;
+		System.out.println(solution(f,k));
+	}
+	public static int solution(int[] food_times, long k) {
+		boolean v[]=new boolean[food_times.length];
+		
+		ArrayList<Food>al=new ArrayList<>();
+		long sum=0;
+        for(int i=0; i<food_times.length; i++) {
+        	al.add(new Food(i,food_times[i]));
+        	sum+=food_times[i];
+        }
+        if(sum<=k)return -1;
+        long min=0; 
+        int n=food_times.length;
+        Collections.sort(al,(o1,o2)->o1.remain-o2.remain);
+        while(!al.isEmpty()) {
+        	if(n*(al.get(0).remain-min)<=k) {
+        		
+        		k-=n*(al.get(0).remain-min);
+        		min=al.get(0).remain;
+        		al.remove(0);
+        		n--;
+        	}else {
+        		break;
+        	}
+        	
+        }
+        
+        Collections.sort(al,(o1,o2)->o1.idx-o2.idx);
+        
+        return al.get((int)(k%n)).idx+1;
+        
+        
+    }
+	static class Food{
+		int idx;
+		int remain;
+		public Food(int idx, int remain) {
+			super();
+			this.idx = idx;
+			this.remain = remain;
+		}
+		
+	}
+}
