0a1198196802a9b9b5402a7b064c278a914d667c
kenny <kenny397@naver.com>
2021-08-25 15:21:46 +0900

    아기상어

--- algo/src/algo/C16236.java
+++ algo/src/algo/C16236.java
@@ -1,121 +1,108 @@
 package algo;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class C16236 {
-static int[] dx= {-1,0,0,1};
-static int[] dy= {0,-1,1,0};
-static int n;
-static int size;
-static int[][]d;
-static int zx;
-static int zy;
-static int xx;
-static int yy;
-static int[][]map;
-static int weight;
-static boolean [][]v;
-	public static void main(String[] args) {
+	static int[][] map;
+	static int ret;
+	static int currentx;
+	static int currenty;
+	static int size = 2;
+	static int point = 0;
+	static int[] dx = { -1, 0, 0, 1 };
+	static int[] dy = { 0, -1, 1, 0 };
+
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		n=sc.nextInt();
-		map=new int[n][n];
-		size=2;
-		 zx=0; zy=0;
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				map[i][j]=sc.nextInt();
-				if(map[i][j]==9) {
-					xx=i; yy=j;
-					map[i][j]=0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		map = new int[n][n];
+		for (int i = 0; i < map.length; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < map.length; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 9) {
+					currentx = i;
+					currenty = j;
 				}
 			}
 		}
-//		
-		
-//		
-		int sum=0;
-		int eatcount=0;
-		while(true) {
-			weight=Integer.MAX_VALUE;
-			zx=Integer.MAX_VALUE;
-			zy=Integer.MAX_VALUE;
-			
-			bfs(xx,yy);
-			
-			if(zx==Integer.MAX_VALUE||zy==Integer.MAX_VALUE)break;
-			xx=zx;
-			yy=zy;
-			sum+=weight;
-			eatcount++;
-			if(eatcount==size) {
+		int max = 0;
+		while (true) {
+			max = ret;
+			bfs(currentx, currenty);
+			if (max == ret)
+				break;
+		}
+		System.out.println(ret);
+	}
+
+	private static void bfs(int startx, int starty) {
+		// TODO Auto-generated method stub
+		boolean[][] v = new boolean[map.length][map.length];
+		ArrayList<Node> al = new ArrayList<>();
+		Queue<Node> q = new LinkedList<>();
+		int max = Integer.MAX_VALUE;
+		q.add(new Node(startx, starty, 0));
+		v[startx][starty] = true;
+		map[startx][starty] = 0;
+		while (!q.isEmpty()) {
+			Node node = q.poll();
+			if (max < node.d)
+				break;
+			if (map[node.x][node.y] != 0 && map[node.x][node.y] < size && node.d <= max) {
+				max = node.d;
+				al.add(new Node(node.x, node.y, node.d));
+			}
+			for (int i = 0; i < 4; i++) {
+				int x = node.x + dx[i];
+				int y = node.y + dy[i];
+				if (x < 0 || y < 0 || x >= map.length || y >= map.length)
+					continue;
+				if (!v[x][y] && map[x][y] <= size) {
+					v[x][y] = true;
+					q.add(new Node(x, y, node.d + 1));
+				}
+			}
+
+		}
+		Collections.sort(al, (o1, o2) -> {
+			if (o1.x == o2.x) {
+				return o1.y - o2.y;
+			}
+			return o1.x - o2.x;
+		});
+		if (!al.isEmpty()) {
+			ret += al.get(0).d;
+			point++;
+			currentx = al.get(0).x;
+			currenty = al.get(0).y;
+			map[currentx][currenty] = 0;
+			if (point == size) {
+				point = 0;
 				size++;
-				eatcount=0;
 			}
 		}
-		System.out.println(sum);
-	}
-	
-	static void bfs(int x,int y) {
-		Queue<Node> q=new LinkedList<>();
-		
 
-		v=new boolean[n][n];
-		v[x][y]=true;
-		
-		map[x][y]=0;
-		q.add(new Node(x,y,0));
-		while(!q.isEmpty()) {
-			Node node=q.poll();
-			int cx=node.x;
-			int cy=node.y;
-			
-			for(int i=0; i<4; i++) {
-			int nx=cx+dx[i];
-			int ny=cy+dy[i];
-			if(nx<0||ny<0||nx>=n||ny>=n)continue;
-			if(map[nx][ny]>size)continue;
-			if(map[nx][ny]>0&&map[nx][ny]<size) {
-				if(weight>node.w+1) {
-					zx=nx;zy=ny;
-					weight=node.w+1;
-				}else if( weight==node.w+1) {
-					if(zx==nx) {
-						if(zy>ny) {
-							zy=ny;
-							zx=nx;
-						}
-					}else if(zx>nx) {
-						zx=nx;
-						zy=ny;
-					}
-				}
-				
-				
-				
-				
-			}
-			if(!v[nx][ny]) {
-				v[nx][ny]=true;
-				q.offer(new Node(nx,ny,node.w+1));
-			}
-			}
+	}
+
+	static class Node {
+		int x, y, d;
+
+		public Node(int x, int y, int d) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.d = d;
 		}
-		return;
-		
-	}
-	
 
-}
-class Node{
-	int x;
-	int y;
-	int w;
-	Node(int x, int y,int w){
-		this.x=x;
-		this.y=y;
-		this.w=w;
 	}
 }
