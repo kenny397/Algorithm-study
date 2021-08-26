4cb1a616a71a9adf316cc006fc5483e69e5beb08
kenny <kenny397@naver.com>
2021-08-26 21:57:17 +0900

    study

--- algo/src/programmers/PS_무지의먹방라이브.java
+++ algo/src/programmers/PS_무지의먹방라이브.java
@@ -2,6 +2,7 @@ package programmers;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 
 public class PS_무지의먹방라이브 {
 
@@ -14,7 +15,7 @@ public class PS_무지의먹방라이브 {
 	public static int solution(int[] food_times, long k) {
 		boolean v[]=new boolean[food_times.length];
 		
-		ArrayList<Food>al=new ArrayList<>();
+		LinkedList<Food>al=new LinkedList<>();
 		long sum=0;
         for(int i=0; i<food_times.length; i++) {
         	al.add(new Food(i,food_times[i]));
@@ -25,20 +26,16 @@ public class PS_무지의먹방라이브 {
         int n=food_times.length;
         Collections.sort(al,(o1,o2)->o1.remain-o2.remain);
         while(!al.isEmpty()) {
-        	if(n*(al.get(0).remain-min)<=k) {
-        		
+        	if(n*(al.get(0).remain-min)<=k) {	
         		k-=n*(al.get(0).remain-min);
         		min=al.get(0).remain;
         		al.remove(0);
         		n--;
         	}else {
         		break;
-        	}
-        	
+        	}        	
         }
-        
         Collections.sort(al,(o1,o2)->o1.idx-o2.idx);
-        
         return al.get((int)(k%n)).idx+1;
         
         
