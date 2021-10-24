355173a82f2a63d5ab79a483e720885a2f6c4f80
kenny <kenny397@naver.com>
2021-10-25 00:15:25 +0900

    숨바꼭질3

---
+++ algo/src/algo/C13549_dij.java
@@ -0,0 +1,17 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class C13549_dij {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		int n=Integer.parseInt(st.nextToken());
+		int k=Integer.parseInt(st.nextToken());
+	}
+
+}
