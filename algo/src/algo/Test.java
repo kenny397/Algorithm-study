2a0743ee2bff053ed57a292b8e64b9b738a59372
kenny <kenny397@naver.com>
2021-12-14 23:41:44 +0900

    Test

--- algo/src/algo/Test.java
+++ algo/src/algo/Test.java
@@ -1,10 +1,31 @@
 package algo;
 
-public class Test {
+import java.util.Set;
+import java.util.HashSet;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("lasart");
+class Sup{
+	String x= "sup";
+	public void method() {
+		System.out.println("supmethod");
 	}
+}
+class Sub extends Sup{
+	String x="sub";
+	@Override
+	public void method() {
+		System.out.println("submethod");
+	}
+}
 
+public class Test {
+	public static void main(String[] args) {
+		Sub sub=new Sub();
+		System.out.println(sub.x);
+		sub.method();
+		Sup sup=new Sub();
+		System.out.println(sup.x);
+		sup.method();
+		
+
+	}
 }
